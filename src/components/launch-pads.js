17787255daa5ad02f0d13e05c0fd9953402867e6
Danddy <danddycandy@gmail.com>
2023-12-13 21:20:42 -0500

    feat: minor update

--- src/components/launch-pads.js
+++ src/components/launch-pads.js
@@ -1,5 +1,5 @@
 import { Badge, Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, IconButton, Input, SimpleGrid, Text } from "@chakra-ui/react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Error from "./error";
 import Breadcrumbs from "./breadcrumbs";
